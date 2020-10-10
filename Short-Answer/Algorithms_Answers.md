5e59089a4279ce31c27e41a80090a40df439587e
Nathan <nate1898@gmail.com>
2020-10-09 23:01:23 -0400

    Met mvp and stretch

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -3,13 +3,21 @@
 ## Exercise I
 
 a)
-
+ o(n log n)
 
 b)
-
+    sum = 0
+    for i in range(n):
+      j = 1
+      while j < n:
+        j *= 2
+        sum += 1
+    (n * n /2)
 
 c)
-
+    o(n)
 ## Exercise II
+    Divide n by 2 if it doesnt break then add half of the half again until it breaks then go down until it stops breaking and the value prev is the value that the eggs stop breaking at, otherwise divide by half of half n until it stops breaking then add one till it breaks and thats the number you need
 
+    this would be O(n log n) and would be space complexity of o(1)
 
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,21 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
     # TBC
+    n1 = len(word)
+    if(n1 == 0 or n1 < 2):
+        return 0
+
+    def recurses(word, value):
+        if len(word) < 2:
+            return value
+        if(word[0:2] == "th"):
+            value += 1
+        return recurses(word[1:], value)
     
-    pass
+    value = recurses(word, 0)
+    return value
+    """
+    for real thou why cant i just do this lmao
+    return word.count("th")
+    """
